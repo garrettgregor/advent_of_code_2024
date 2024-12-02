6ab66f2a4ad1c3786709b0a33753a7de4d6c7bb4
Garrett Gregor <garrett.gregor@gmail.com>
2024-12-01 20:54:55 -0700

    docs: add basic documentation for methods

--- day_1/lib/historian_hysteria.rb
+++ day_1/lib/historian_hysteria.rb
@@ -12,6 +12,7 @@ class HistorianHysteria
     @right_list = list.map { |line| line[1].to_i }.sort
   end
 
+  # Returns a list of the differences between each correlated element between lists.
   def differences
     results = []
 
@@ -22,10 +23,13 @@ class HistorianHysteria
     results
   end
 
+  # Returns the sum of the list of the differences between each correlated element between lists.
   def sum_of_differences
     differences.sum
   end
 
+  # Returns the "similarity score" dictionary as defined by the number of times an element on the left list
+  # appears on the right list
   def similarity_scores
     scores = {}
 
@@ -36,12 +40,14 @@ class HistorianHysteria
     scores
   end
 
+  # Returns a list of the left list elements multiplied by their corresponding "similarity scores".
   def multiplied_scores
     left_list.map do |left_num|
       left_num * similarity_scores[left_num.to_s]
     end
   end
 
+  # Returns a sum of list of left list elements multiplied by their corresponding "similarity scores".
   def sum_of_multiplied_scores
     multiplied_scores.sum
   end
