fb1293bebbdabc35f165ff6b44e25ee4206888c6
Garrett Gregor <garrett.gregor@gmail.com>
2024-12-01 20:03:13 -0700

    feat: HistorianHysteria #initialize, #left_list, #right_list, #differences and #sum_of_differences

--- day_1/lib/historian_hysteria.rb
+++ day_1/lib/historian_hysteria.rb
@@ -1,10 +1,26 @@
 class HistorianHysteria
-  attr_reader :list
+  attr_reader :list, :left_list, :right_list
 
   def initialize(file_path)
     @list = File.open(file_path).map do |line|
-      require 'pry'; binding.pry
+      line.strip.split(/\D/).reject { |c| c.empty? }
     end
+
+    @left_list = list.map { |line| line[0].to_i }.sort
+    @right_list = list.map { |line| line[1].to_i }.sort
   end
 
+  def differences
+    results = []
+
+    left_list.each_with_index do |left_el, left_i|
+      results << (left_el-right_list[left_i]).abs
+    end
+
+    results
+  end
+
+  def sum_of_differences
+    differences.sum
+  end
 end
