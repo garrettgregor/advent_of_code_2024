3600054ac3332a4d96f28651cb72be48d12ae1c8
Garrett Gregor <118634754+garrettgregor@users.noreply.github.com>
2025-08-20 12:27:11 -0600

    Day 5 part 1 (#5)
    * chores: update rubocop config
    
    * chores: set local ruby version
    
    * test: PrintQueue#initialize
    
    * feat: PrintQueue#initialize
    
    * test: PrintQueue#parsed_results
    
    * feat: PrintQueue#parsed_results
    
    * fix: replace accidentatlly removed #initialize test
    
    * test: PrintQueue#parsed_updates
    
    * feat: PrintQueue#parsed_updates
    
    * test: PrintQueue#follows_rules?
    
    * feat: PrintQueue#follows_rules?
    
    * test: PrintQueue#separate_correct_and_incorrect_updates
    
    * feat: PrintQueue#separate_correct_and_incorrect_updates
    
    * test: PrintQueue#find_correct_middles
    
    * feat: PrintQueue#find_correct_middles
    
    * test: PrintQueue#sum_correct_middles and edit incorrect it blocks
    
    * feat: PrintQueue#sum_correct_middles
    
    * feat: solve part 1

---
+++ day_5/lib/print_queue.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+class PrintQueue
+  attr_reader :raw_rules, :raw_updates, :correct_updates, :incorrect_updates
+
+  def initialize(file_path)
+    @raw_rules = File.readlines(file_path, chomp: true).filter_map do |line|
+      line if line.include?("|")
+    end
+    @raw_updates = File.readlines(file_path, chomp: true).filter_map do |line|
+      line if line.include?(",")
+    end
+    @correct_updates = []
+    @incorrect_updates = []
+  end
+
+  def parsed_rules
+    results = {}
+
+    raw_rules.each do |rule|
+      key, value = rule.strip.split("|")
+      if !results[key.to_i]
+        results[key.to_i] = [value.to_i]
+      else
+        results[key.to_i] << value.to_i
+      end
+    end
+
+    results
+  end
+
+  def parsed_updates
+    results = []
+
+    raw_updates.each do |update|
+      results << update.split(",").map(&:to_i)
+    end
+
+    results
+  end
+
+  def follows_rules?(update) # rubocop:disable Metrics/MethodLength
+    update = update.dup
+
+    n = update.length - 1
+
+    n.times do
+      rule_key = update.shift
+      subsequent_set = update
+      elements_that_show_follow_if_they_exist = parsed_rules[rule_key]
+
+      subsequent_set.each do |el|
+        # use safe nav here in the event a rule key does not exist
+        return false unless elements_that_show_follow_if_they_exist&.include?(el)
+      end
+    end
+
+    true
+  end
+
+  def separate_correct_and_incorrect_updates
+    parsed_updates.each do |update|
+      follows_rules?(update) ? correct_updates << update : incorrect_updates << update
+    end
+  end
+
+  def find_correct_middles
+    separate_correct_and_incorrect_updates
+
+    correct_updates.map do |update|
+      update[update.length / 2]
+    end
+  end
+
+  def sum_correct_middles
+    find_correct_middles.sum
+  end
+end
---
+++ day_5/spec/print_queue_spec.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+require "../spec_helper"
+require "print_queue"
+
+RSpec.describe PrintQueue do
+  describe "instance_methods" do
+    context "when solving part 1" do
+      let!(:file_path) { "./fixtures/example_1.txt" }
+      let!(:instance) { described_class.new(file_path) }
+
+      describe "#initialize" do
+        it "starts with rules and update, and space for correct and incorrect updates" do
+          raw_rules = ["47|53", "97|13", "97|61", "97|47", "75|29", "61|13", "75|53", "29|13", "97|29", "53|29",
+                       "61|53", "97|53", "61|29", "47|13", "75|47", "97|75", "47|61", "75|61", "47|29", "75|13", "53|13"] # rubocop:disable Layout/LineLength
+          raw_updates = ["75,47,61,53,29", "97,61,53,29,13", "75,29,13", "75,97,47,61,53", "61,13,29", "97,13,75,29,47"]
+          correct_updates = []
+          incorrect_updates = []
+
+          expect(instance).to be_a(described_class)
+          expect(instance.raw_rules).to eq(raw_rules)
+          expect(instance.raw_updates).to eq(raw_updates)
+          expect(instance.correct_updates).to eq(correct_updates)
+          expect(instance.incorrect_updates).to eq(incorrect_updates)
+        end
+      end
+
+      describe "#parsed_rules" do
+        it "parses the raw rules" do
+          results = {
+            47 => [53, 13, 61, 29],
+            97 => [13, 61, 47, 29, 53, 75],
+            75 => [29, 53, 47, 61, 13],
+            61 => [13, 53, 29],
+            29 => [13],
+            53 => [29, 13]
+          }
+
+          expect(instance.parsed_rules).to eq(results)
+        end
+      end
+
+      describe "#parsed_updates" do
+        it "parses the raw updates and keeps the lines preserved" do
+          results = [
+            [75, 47, 61, 53, 29],
+            [97, 61, 53, 29, 13],
+            [75, 29, 13],
+            [75, 97, 47, 61, 53],
+            [61, 13, 29],
+            [97, 13, 75, 29, 47]
+          ]
+
+          expect(instance.parsed_updates).to eq(results)
+        end
+      end
+
+      describe "#follows_rules?" do
+        it "checks whether a given update follows the rules" do
+          update_1 = [75, 47, 61, 53, 29]
+          update_2 = [97, 61, 53, 29, 13]
+          update_3 = [75, 29, 13]
+          update_4 = [75, 97, 47, 61, 53]
+          update_5 = [61, 13, 29]
+          update_6 = [97, 13, 75, 29, 47]
+
+          expect(instance.follows_rules?(update_1)).to be(true)
+          expect(instance.follows_rules?(update_2)).to be(true)
+          expect(instance.follows_rules?(update_3)).to be(true)
+          expect(instance.follows_rules?(update_4)).to be(false)
+          expect(instance.follows_rules?(update_5)).to be(false)
+          expect(instance.follows_rules?(update_6)).to be(false)
+        end
+      end
+
+      describe "#separate_correct_and_incorrect_updates" do
+        it "distributes updates into correct and incorrect update lists" do
+          update_1 = [75, 47, 61, 53, 29]
+          update_2 = [97, 61, 53, 29, 13]
+          update_3 = [75, 29, 13]
+          update_4 = [75, 97, 47, 61, 53]
+          update_5 = [61, 13, 29]
+          update_6 = [97, 13, 75, 29, 47]
+
+          correct_results = [update_1, update_2, update_3]
+          incorrect_results = [update_4, update_5, update_6]
+
+          instance.separate_correct_and_incorrect_updates
+
+          expect(instance.correct_updates).to eq(correct_results)
+          expect(instance.incorrect_updates).to eq(incorrect_results)
+        end
+      end
+
+      describe "#find_correct_middles" do
+        it "finds the middle elements of the correct updates" do
+          results = [61, 53, 29]
+
+          expect(instance.find_correct_middles).to eq(results)
+        end
+      end
+
+      describe "#sum_correct_middles" do
+        it "sums up the middle elements" do
+          result = 143
+
+          expect(instance.sum_correct_middles).to eq(result)
+        end
+      end
+    end
+  end
+end
